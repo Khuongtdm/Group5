f7ba2816a6fcfd7b6ab5d82c6cfee558774b421f
theMackabu <theMackabu@gmail.com>
2024-09-14 16:02:41 -0700

    add summary page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,17 +6,12 @@ import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { ArrowDownCircleIcon, ArrowPathIcon, ArrowUpCircleIcon, EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
 
 const stats = [
-	{ name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
-	{ name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
-	{ name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
-	{ name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' }
+ 	{ name: 'Subscriptions', value: '3' },
+	{ name: 'Monthly Cost', value: '$29.34' },
+	{ name: 'Yearly Cost', value: '$352.08' },
+	{ name: 'Monthly Salary', value: '$2,513.00' },
+  { name: 'Yearly Income', value: '$30,156.00' },
 ];
-
-const statuses = {
-	Paid: 'text-green-700 bg-green-50 ring-green-600/20',
-	Withdraw: 'text-gray-600 bg-gray-50 ring-gray-500/10',
-	Overdue: 'text-red-700 bg-red-50 ring-red-600/10'
-};
 
 const days = [
 	{
@@ -29,7 +24,6 @@ const days = [
 				href: '#',
 				amount: '$7,600.00 USD',
 				tax: '$500.00',
-				status: 'Paid',
 				client: 'Reform',
 				description: 'Website redesign',
 				icon: ArrowUpCircleIcon
@@ -39,7 +33,6 @@ const days = [
 				invoiceNumber: '00011',
 				href: '#',
 				amount: '$10,000.00 USD',
-				status: 'Withdraw',
 				client: 'Tom Cook',
 				description: 'Salary',
 				icon: ArrowDownCircleIcon
@@ -50,7 +43,6 @@ const days = [
 				href: '#',
 				amount: '$2,000.00 USD',
 				tax: '$130.00',
-				status: 'Overdue',
 				client: 'Tuple',
 				description: 'Logo design',
 				icon: ArrowPathIcon
@@ -67,7 +59,6 @@ const days = [
 				href: '#',
 				amount: '$14,000.00 USD',
 				tax: '$900.00',
-				status: 'Paid',
 				client: 'SavvyCal',
 				description: 'Website redesign',
 				icon: ArrowUpCircleIcon
@@ -76,32 +67,32 @@ const days = [
 	}
 ];
 
-const clients = [
+const subscriptions = [
 	{
 		id: 1,
-		name: 'Tuple',
+		name: 'Netflix',
 		imageUrl: 'https://tailwindui.com/img/logos/48x48/tuple.svg',
-		lastInvoice: { date: 'December 13, 2022', dateTime: '2022-12-13', amount: '$2,000.00', status: 'Overdue' }
+		lastInvoice: { date: 'December 13, 2022', dateTime: '2022-12-13', amount: '$14.99/m' }
 	},
 	{
 		id: 2,
-		name: 'SavvyCal',
+		name: 'Google One',
 		imageUrl: 'https://tailwindui.com/img/logos/48x48/savvycal.svg',
-		lastInvoice: { date: 'January 22, 2023', dateTime: '2023-01-22', amount: '$14,000.00', status: 'Paid' }
+		lastInvoice: { date: 'January 22, 2023', dateTime: '2023-01-22', amount: '$1.99/m' }
 	},
 	{
 		id: 3,
-		name: 'Reform',
+		name: 'Twitch',
 		imageUrl: 'https://tailwindui.com/img/logos/48x48/reform.svg',
-		lastInvoice: { date: 'January 23, 2023', dateTime: '2023-01-23', amount: '$7,600.00', status: 'Paid' }
+		lastInvoice: { date: 'January 23, 2023', dateTime: '2023-01-23', amount: '$11.99/m' }
 	}
 ];
 
 const Home = (props: { title: string }) => (
 	<PageContentBlock title={props.title}>
 		<div className="relative isolate overflow-hidden">
-			<div className="border-b border-b-gray-900/10 lg:border-t lg:border-t-gray-900/5">
-				<dl className="mx-auto grid max-w-7xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:px-2 xl:px-0">
+			<div className="border-b border-b-gray-900/10">
+				<dl className="mx-auto grid max-w-7xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 lg:px-2 xl:px-0">
 					{stats.map((stat, statIdx) => (
 						<div
 							key={stat.name}
@@ -110,7 +101,6 @@ const Home = (props: { title: string }) => (
 								'flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 border-t border-gray-900/5 px-4 py-10 sm:px-6 lg:border-t-0 xl:px-8'
 							)}>
 							<dt className="text-sm font-medium leading-6 text-gray-500">{stat.name}</dt>
-							<dd className={classNames(stat.changeType === 'negative' ? 'text-rose-600' : 'text-gray-700', 'text-xs font-medium')}>{stat.change}</dd>
 							<dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">{stat.value}</dd>
 						</div>
 					))}
@@ -132,7 +122,67 @@ const Home = (props: { title: string }) => (
 		</div>
 
 		<div className="space-y-16 py-16 xl:space-y-20">
-			{/* Recent activity table */}
+			{/* Recent client list*/}
+			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+				<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+					<div className="flex items-center justify-between">
+						<h2 className="text-base font-semibold leading-7 text-gray-900">Subscriptions</h2>
+						<a href="#" className="text-sm font-semibold leading-6 text-brand hover:text-brand/60 transition">
+							View all<span className="sr-only">, clients</span>
+						</a>
+					</div>
+					<ul role="list" className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+						{subscriptions.map((client) => (
+							<li key={client.id} className="overflow-hidden rounded-xl border border-gray-200">
+								<div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
+									<img
+										alt={client.name}
+										src={client.imageUrl}
+										className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+									/>
+									<div className="text-sm font-medium leading-6 text-gray-900">{client.name}</div>
+									<Menu as="div" className="relative ml-auto">
+										<MenuButton className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500 transition">
+											<span className="sr-only">Open options</span>
+											<EllipsisHorizontalIcon aria-hidden="true" className="h-5 w-5" />
+										</MenuButton>
+										<MenuItems
+											transition
+											className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
+											<MenuItem>
+												<a href="#" className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50">
+													View<span className="sr-only">, {client.name}</span>
+												</a>
+											</MenuItem>
+											<MenuItem>
+												<a href="#" className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50">
+													Edit<span className="sr-only">, {client.name}</span>
+												</a>
+											</MenuItem>
+										</MenuItems>
+									</Menu>
+								</div>
+								<dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+									<div className="flex justify-between gap-x-4 py-3">
+										<dt className="text-gray-500">Due</dt>
+										<dd className="text-gray-700">
+											<time dateTime={client.lastInvoice.dateTime}>{client.lastInvoice.date}</time>
+										</dd>
+									</div>
+									<div className="flex justify-between gap-x-4 py-3">
+										<dt className="text-gray-500">Amount</dt>
+										<dd className="flex items-start gap-x-2">
+											<div className="font-medium text-gray-900">{client.lastInvoice.amount}</div>
+										</dd>
+									</div>
+								</dl>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+
+      		{/* Recent activity table */}
 			<div>
 				<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 					<h2 className="mx-auto max-w-2xl text-base font-semibold leading-6 text-gray-900 lg:mx-0 lg:max-w-none">Recent activity</h2>
@@ -166,13 +216,6 @@ const Home = (props: { title: string }) => (
 															<div className="flex-auto">
 																<div className="flex items-start gap-x-3">
 																	<div className="text-sm font-medium leading-6 text-gray-900">{transaction.amount}</div>
-																	<div
-																		className={classNames(
-																			statuses[transaction.status],
-																			'rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset'
-																		)}>
-																		{transaction.status}
-																	</div>
 																</div>
 																{transaction.tax ? <div className="mt-1 text-xs leading-5 text-gray-500">{transaction.tax} tax</div> : null}
 															</div>
@@ -205,69 +248,6 @@ const Home = (props: { title: string }) => (
 							</table>
 						</div>
 					</div>
-				</div>
-			</div>
-
-			{/* Recent client list*/}
-			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-				<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-					<div className="flex items-center justify-between">
-						<h2 className="text-base font-semibold leading-7 text-gray-900">Recent clients</h2>
-						<a href="#" className="text-sm font-semibold leading-6 text-brand hover:text-brand/60 transition">
-							View all<span className="sr-only">, clients</span>
-						</a>
-					</div>
-					<ul role="list" className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
-						{clients.map((client) => (
-							<li key={client.id} className="overflow-hidden rounded-xl border border-gray-200">
-								<div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
-									<img
-										alt={client.name}
-										src={client.imageUrl}
-										className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
-									/>
-									<div className="text-sm font-medium leading-6 text-gray-900">{client.name}</div>
-									<Menu as="div" className="relative ml-auto">
-										<MenuButton className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500 transition">
-											<span className="sr-only">Open options</span>
-											<EllipsisHorizontalIcon aria-hidden="true" className="h-5 w-5" />
-										</MenuButton>
-										<MenuItems
-											transition
-											className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
-											<MenuItem>
-												<a href="#" className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50">
-													View<span className="sr-only">, {client.name}</span>
-												</a>
-											</MenuItem>
-											<MenuItem>
-												<a href="#" className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50">
-													Edit<span className="sr-only">, {client.name}</span>
-												</a>
-											</MenuItem>
-										</MenuItems>
-									</Menu>
-								</div>
-								<dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-									<div className="flex justify-between gap-x-4 py-3">
-										<dt className="text-gray-500">Last invoice</dt>
-										<dd className="text-gray-700">
-											<time dateTime={client.lastInvoice.dateTime}>{client.lastInvoice.date}</time>
-										</dd>
-									</div>
-									<div className="flex justify-between gap-x-4 py-3">
-										<dt className="text-gray-500">Amount</dt>
-										<dd className="flex items-start gap-x-2">
-											<div className="font-medium text-gray-900">{client.lastInvoice.amount}</div>
-											<div className={classNames(statuses[client.lastInvoice.status], 'rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset')}>
-												{client.lastInvoice.status}
-											</div>
-										</dd>
-									</div>
-								</dl>
-							</li>
-						))}
-					</ul>
 				</div>
 			</div>
 		</div>
